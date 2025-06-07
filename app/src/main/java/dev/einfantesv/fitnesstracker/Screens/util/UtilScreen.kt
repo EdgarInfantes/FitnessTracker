07411df44f9cf1582b546dd248b79f1652cf7e30
DRM-Icpna234 <dirtyyr2012@gmail.com>
2025-06-07 14:29:45 -0500

    Improving StepCounterViewModel.kt for a better UI of the step counter and changing the timer for only increasing when the counter increases as well.

--- app/src/main/java/dev/einfantesv/fitnesstracker/Screens/util/UtilScreen.kt
+++ app/src/main/java/dev/einfantesv/fitnesstracker/Screens/util/UtilScreen.kt
@@ -4,25 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -36,7 +29,6 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import dev.einfantesv.fitnesstracker.R
-import org.w3c.dom.Text
 
 
 @Composable
